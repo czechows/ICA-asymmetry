f0ec4e06b87db3fbff26120d3fa3bde90c57ad85
czechows <czechovs@gmail.com>
2017-04-28 13:04:13 +0200

    trying to export

--- R/ICA.r
+++ R/ICA.r
@@ -1,15 +1,19 @@
-Rcpp::sourceCpp('src/ICA.cpp')
-
-myX = matrix( runif(n=24, min=-1, max=1), nrow=8, ncol=3 )
-
-myM = runif( n=3, min=-1, max=1 ) 
-myW = matrix( runif(n=9,min=-1,max=1), nrow=3, ncol=3 )
-
-# returns minimum, writes argmin to myM, myW
-ICA( myX, myM, myW )
-
-print( myM )
-print( myW )
-
+#' The ICA function based on asymmetry
+#'
+#' @return M and W as in the ICAA paper
+#' @keywords Independent Component Analysis
+#' @export
+#' @examples
+#' ICA(data)
+ICAA<-function(myX)
+{
+  myM = runif( n=3, min=-1, max=1 ) 
+  myW = matrix( runif(n=9,min=-1,max=1), nrow=3, ncol=3 )
+  
+  # returns minimum, writes argmin to myM, myW
+  ICA( myX, myM, myW )
+  
+  return(list(myM,myW))
+}
 
 
